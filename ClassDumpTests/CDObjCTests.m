0cd76f5b9f7dbd11e8f88b7fcc9ececb87051728
Leptos <leptos.0.null@gmail.com>
2021-09-25 10:46:55 -0700

    [Tests] Add Objective-C properties with protocol constraints

--- ClassDumpTests/CDObjCTests.m
+++ ClassDumpTests/CDObjCTests.m
@@ -27,6 +27,11 @@
 
 @property (nonatomic) NSString *charlie;
 
+@property (weak, nonatomic) id<NSCacheDelegate> cacheDelegate;
+@property (nonatomic) id<NSProgressReporting, NSFastEnumeration> progressReportingEnumeration;
+@property (nonatomic) NSUUID<NSMutableCopying, NSFastEnumeration> *mutableUUID;
+@property (weak, nonatomic) NSURL<NSDiscardableContent> *discardableURL;
+
 - (id)alfa:(int *const *)alfa error:(inout id *)err;
 
 @end
@@ -60,17 +65,32 @@
     XCTAssert([_model.name isEqualToString:@"CDDemoClass"]);
     XCTAssert(_model.protocols.count == 1);
     XCTAssert(_model.classProperties.count == 1);
-    XCTAssert(_model.ivars.count == 1);
     XCTAssert(_model.classMethods.count == 2);
+    XCTAssert(_model.ivars.count == 5);
 }
 
 - (void)testProtocol {
     CDProtocolModel *protocol = _model.protocols.firstObject;
     XCTAssert([protocol.name isEqualToString:@"CDDemoProtocol"]);
+    
+    NSArray<CDMethodModel *> *requiredInstanceMethods = protocol.requiredInstanceMethods;
+    NSArray<CDMethodModel *> *optionalInstanceMethods = protocol.optionalInstanceMethods;
+    
     XCTAssert(protocol.requiredClassMethods.count == 0);
-    XCTAssert(protocol.requiredInstanceMethods.count == 1);
+    XCTAssert(requiredInstanceMethods.count == 1);
     XCTAssert(protocol.optionalClassMethods.count == 0);
-    XCTAssert(protocol.optionalInstanceMethods.count == 1);
+    XCTAssert(optionalInstanceMethods.count == 1);
+    
+    CDMethodModel *requiredInstanceMethod = requiredInstanceMethods.firstObject;
+    XCTAssert([requiredInstanceMethod.name isEqualToString:@"happenedWithError:"]);
+    XCTAssert(requiredInstanceMethod.argumentTypes.count == 1);
+    XCTAssert([requiredInstanceMethod.argumentTypes[0] isEqualToString:@"id"]);
+    XCTAssert(!requiredInstanceMethod.isClass);
+    
+    CDMethodModel *optionalInstanceMethod = optionalInstanceMethods.firstObject;
+    XCTAssert([optionalInstanceMethod.name isEqualToString:@"happening"]);
+    XCTAssert(optionalInstanceMethod.argumentTypes.count == 0);
+    XCTAssert(!optionalInstanceMethod.isClass);
 }
 
 - (void)testClassProperty {
@@ -82,12 +102,35 @@
 }
 
 - (void)testInstanceProperty {
-    CDPropertyModel *instProp = _model.instanceProperties.firstObject;
-    XCTAssert([instProp.name isEqualToString:@"charlie"]);
-    XCTAssert([instProp.iVar isEqualToString:@"_charlie"]);
-    XCTAssert([instProp.getter isEqualToString:@"charlie"]);
-    XCTAssert([instProp.setter isEqualToString:@"setCharlie:"]);
-    XCTAssert([instProp.type isEqualToString:@"NSString *charlie"]);
+    BOOL found = NO;
+    for (CDPropertyModel *prop in _model.instanceProperties) {
+        if ([prop.name isEqualToString:@"charlie"]) {
+            XCTAssert(!found, @"Found property multiple times");
+            found = YES;
+            
+            XCTAssert([prop.iVar isEqualToString:@"_charlie"]);
+            XCTAssert([prop.getter isEqualToString:@"charlie"]);
+            XCTAssert([prop.setter isEqualToString:@"setCharlie:"]);
+            XCTAssert([prop.type isEqualToString:@"NSString *charlie"]);
+        }
+    }
+    XCTAssert(found, @"Failed to find property");
+}
+
+- (void)testPropertyProtocols {
+    for (CDPropertyModel *prop in _model.instanceProperties) {
+        if ([prop.name isEqualToString:@"charlie"]) {
+            XCTAssert([prop.type isEqualToString:@"NSString *charlie"]);
+        } else if ([prop.name isEqualToString:@"cacheDelegate"]) {
+            XCTAssert([prop.type isEqualToString:@"id<NSCacheDelegate> cacheDelegate"]);
+        } else if ([prop.name isEqualToString:@"progressReportingEnumeration"]) {
+            XCTAssert([prop.type isEqualToString:@"id<NSProgressReporting, NSFastEnumeration> progressReportingEnumeration"]);
+        } else if ([prop.name isEqualToString:@"mutableUUID"]) {
+            XCTAssert([prop.type isEqualToString:@"NSUUID<NSMutableCopying, NSFastEnumeration> *mutableUUID"]);
+        } else if ([prop.name isEqualToString:@"discardableURL"]) {
+            XCTAssert([prop.type isEqualToString:@"NSURL<NSDiscardableContent> *discardableURL"]);
+        }
+    }
 }
 
 - (void)testMethod {
@@ -97,6 +140,7 @@
             XCTAssert(!found, @"Found method multiple times");
             found = YES;
             
+            XCTAssert(mthd.argumentTypes.count == 2);
             XCTAssert([mthd.argumentTypes[0] isEqualToString:@"int **"]);
             XCTAssert([mthd.argumentTypes[1] isEqualToString:@"inout id *"]);
             XCTAssert([mthd.returnType isEqualToString:@"id"]);
@@ -107,9 +151,16 @@
 }
 
 - (void)testIvar {
-    CDIvarModel *ivar = _model.ivars.firstObject;
-    XCTAssert([ivar.name isEqualToString:@"_charlie"]);
-    XCTAssert([ivar.line isEqualToString:@"NSString *_charlie"]);
+    BOOL found = NO;
+    for (CDIvarModel *ivar in _model.ivars) {
+        if ([ivar.name isEqualToString:@"_charlie"]) {
+            XCTAssert(!found, @"Found ivar multiple times");
+            found = YES;
+            
+            XCTAssert([ivar.line isEqualToString:@"NSString *_charlie"]);
+        }
+    }
+    XCTAssert(found, @"Failed to find ivar");
 }
 
 @end
