6f1dccc0371db322370d622cc6e1587d3e9bed27
Leptos <leptos.0.null@gmail.com>
2023-01-03 14:26:04 -0800

    [CDSemanticString] Improve performance by using a linked list

--- ClassDump/Models/CDSemanticString.m
+++ ClassDump/Models/CDSemanticString.m
@@ -9,28 +9,45 @@
 #import "CDSemanticString.h"
 
 @interface CDSemanticStringStaple : NSObject
+
 @property (strong, nonatomic) NSString *string;
 @property (nonatomic) CDSemanticType type;
+
+@property (strong, nonatomic) CDSemanticStringStaple *next;
+
 @end
 
 @implementation CDSemanticStringStaple
 @end
 
-
+// This implementation is designed to be perfomance sensitive.
+// Since we don't require random-access, the string is composed by a linked list.
 @implementation CDSemanticString {
-    NSMutableArray<CDSemanticStringStaple *> *_components;
+    CDSemanticStringStaple *_head;
+    CDSemanticStringStaple *_tail;
 }
 
 - (instancetype)init {
     if (self = [super init]) {
         _length = 0;
-        _components = [NSMutableArray array];
+        _head = nil;
+        _tail = nil;
     }
     return self;
 }
 
 - (void)appendSemanticString:(CDSemanticString *)semanticString {
-    [_components addObjectsFromArray:semanticString->_components];
+    if (_head == nil) {
+        _head = semanticString->_head;
+        _tail = semanticString->_tail;
+        _length = semanticString->_length;
+        return;
+    }
+    CDSemanticStringStaple *appendTail = semanticString->_tail;
+    if (appendTail != nil) {
+        _tail.next = semanticString->_head;
+        _tail = appendTail;
+    }
     _length += semanticString.length;
 }
 
@@ -39,7 +56,14 @@
         CDSemanticStringStaple *staple = [CDSemanticStringStaple new];
         staple.string = string;
         staple.type = type;
-        [_components addObject:staple];
+        staple.next = nil;
+        
+        if (_head == nil) {
+            _head = staple;
+        }
+        _tail.next = staple;
+        _tail = staple;
+        
         _length += string.length;
     }
 }
@@ -47,18 +71,18 @@
 - (BOOL)endWithChar:(char)character {
     char *bytes = &character;
     NSString *suffix = [[NSString alloc] initWithBytesNoCopy:bytes length:1 encoding:NSASCIIStringEncoding freeWhenDone:NO];
-    return [_components.lastObject.string hasSuffix:suffix];
+    return [_tail.string hasSuffix:suffix];
 }
 
 - (void)enumerateTypesUsingBlock:(void (NS_NOESCAPE ^)(NSString *string, CDSemanticType type))block {
-    for (CDSemanticStringStaple *staple in _components) {
+    for (CDSemanticStringStaple *staple = _head; staple != nil; staple = staple.next) {
         block(staple.string, staple.type);
     }
 }
 
 - (NSString *)string {
     NSMutableString *build = [NSMutableString string];
-    for (CDSemanticStringStaple *staple in _components) {
+    for (CDSemanticStringStaple *staple = _head; staple != nil; staple = staple.next) {
         [build appendString:staple.string];
     }
     return [build copy];
